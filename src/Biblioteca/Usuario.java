84d3fc35f3d563b50d3e60571094faeee89b6642
Karlita <Karlita@DESKTOP-ID66OAK>
2021-07-19 18:37:39 -0500

    Mi POO

--- src/Biblioteca/Usuario.java
+++ src/Biblioteca/Usuario.java
@@ -1,5 +1,36 @@
 package Biblioteca;
 
 public class Usuario {
-
+	
+	String nombre;
+	int edad;
+	int Ncedula;
+	
+	String getNombre() {
+		return nombre;
+		
+	}
+	
+	void setNombre(String nombre) {
+		this.nombre=nombre;
+		
+	}
+	int getEdad() {
+		return edad;	
+		
+	}
+	
+	void setEdad(int edad) {
+		this.edad=edad;
+		
+	}
+	int getNcedula() {
+		return Ncedula;
+		
+	}
+	
+	void SetNcedula(int Ncedula) {
+		this.Ncedula=Ncedula;
+		
+	}
 }
