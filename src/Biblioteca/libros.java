8c1ae39894e2d1b99052d733a9d5e5fe0f0388b4
Karlita <Karlita@DESKTOP-ID66OAK>
2021-07-19 18:42:44 -0500

    MI POO

--- src/Biblioteca/libros.java
+++ src/Biblioteca/libros.java
@@ -1,5 +1,26 @@
 package Biblioteca;
 
 public class libros {
+	
+
+	private String Nombre;
+	private int Codigo;
+	private int ISBN;
+	
+	
+	void tipo(String color, String tamano) {
+		int valor=80;
+		String valor2=color+tamano;
+		
+	}
+	void tipo(String color) {
+		int codigo=23456;
+		int valor=80;
+		String arbog=color+6;
+		
+	}
 
 }
+
+
+
