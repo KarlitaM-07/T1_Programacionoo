9429e5150e4f883187f0e97469778b0bcbc94445
Karlita <Karlita@DESKTOP-ID66OAK>
2021-07-21 18:49:40 -0500

    Datos

---
+++ src/Tipos/Datos/E/MainTiposDeDatos.java
@@ -0,0 +1,28 @@
+package Tipos.Datos.E;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		short cantidadEstudiantes =1000;
+		
+		byte entradaCircuito = 0;
+		
+		int cantidadCiudades = 1000000;
+		
+		long cantidadMundo = 1000000000;
+		
+		float sueldo = 1000.56f;
+		
+		double saldoBanco = 100000000.67;
+		
+		char opcionMenu = 'C';
+		
+		boolean casado = true;
+		
+
+	}
+
+}
