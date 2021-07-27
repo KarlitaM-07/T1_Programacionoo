dfb88ece6b2bf7ac4eaad561ea2f1b844980a9a1
Karlita <Karlita@DESKTOP-ID66OAK>
2021-07-26 20:04:53 -0500

    Prueba1

---
+++ src/chipantasi/Main.java
@@ -0,0 +1,26 @@
+package chipantasi;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Numero positivo");
+		System.out.println("La suma de todos los numeros: ");
+		System.out.println("El promedio de todos los numeros: ");
+		Scanner teclado = new Scanner(System.in);
+		int A = teclado.nextInt();
+		int B = teclado.nextInt();
+		if(A>=10) {
+			System.out.println("ingrese un numero ");
+		}
+		else {
+			System.out.println("La suma de todos los numeros: ");
+		}
+			
+		
+	}
+}
