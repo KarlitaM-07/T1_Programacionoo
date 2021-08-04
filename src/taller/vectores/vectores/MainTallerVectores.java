99ce917d49aa024338e52671c0da20082089e871
Karlita <Karlita@DESKTOP-ID66OAK>
2021-08-04 17:03:14 -0500

    Tarea Vectores

---
+++ src/taller/vectores/vectores/MainTallerVectores.java
@@ -0,0 +1,126 @@
+package taller.vectores.vectores;
+
+public class MainTallerVectores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. Declare un vector X[8]
+		
+		int miVector[]=new int [8];
+		
+		//2. Declara un vector Y[3] y asignar a la posicion Y[3] y asignar a la posicion 2 el valor A
+		
+		char Y[]=new char [3];
+		Y [2]= 'A';
+		
+		
+		// 3. Declarar un vector A [5] y asignar a la posicion A[0] el valor 25 a la posicion A[3] 
+		// el valor de 49 y a la posicion A[2] el valor de la posicion A[0]
+		
+		int A []=new int [5];
+		A [0]= 25;
+		A [3]= 49;
+		A [2]= 25;
+		
+			
+		// 4. Declarar un vector B[10] y asignar a la posicion B[0] el valor del vector A[3] del ejercicio anterior
+		
+		int B []= new int [10];
+		B [0]= 49;
+		
+		
+		// 5. Declarar un vector Z(12) y asignar a todas sus posiciones valores impares que inician desde uno y son secuenciales
+		
+		int Z []= new int [12];
+		
+		Z [0]= 1;
+		Z [1]= 2;
+		Z [2]= 3;
+		Z [3]= 4;
+		Z [4]= 5;
+		Z [5]= 6;
+		Z [6]= 7;
+		Z [7]= 8;
+		Z [8]= 9;
+		Z [9]= 10;
+		Z [10]= 11;
+		Z [11]= 12;
+		
+		
+		//6. Declare un vector Y[10] y asignar a todas las posiciones valores impares que inician en 1 y son secuenciales 
+		
+         int M []= new int [10];
+		
+		M [0]= 1;
+		M [1]= 3;
+		M [2]= 5;
+		M [3]= 7;
+		M [4]= 9;
+		M [5]= 11;
+		M [6]= 13;
+		M [7]= 15;
+		M [8]= 17;
+		M [9]= 19;
+		
+		// 7. Declare un vector H[10] y asignar solo a las posiciones impares las letras que inician desde
+		// la A secuencialmente 
+	
+		char H[]=new char [10];
+		H [1]= 'A';
+		H [3]= 'B';
+		H [5]= 'C';
+		H [7]= 'D';
+		H [9]= 'E';
+		
+		// 8. Declare un vector F[10] y en base al ejercicio anterior asignar los valores de la siguiente
+		// manera F[0] el valor de 3 F[3] el valor de 2 F[4] el valor de 6 F[7] el valor de 0 F[8] el valor de 1
+		// F[9] el valor de 9
+		
+		int F []=new int [10];
+		F [0]= 3;
+		F [3]= 2;
+		F [4]= 6;
+		F [7]= 0;
+		F [8]= 1;
+		F [9]= 9;
+		
+		
+		// 9. Declarar un vector L[15] y asignar valoresen base a lo siguiente L[0] el promedio de 3,5,7,9 L[4] la suma de 5,7,9,10 
+		// L[7] la multiplicacion de 6,7,3 L[10] la division de 10/2
+		
+		int L []=new int [15];
+		F [0]= 6;
+		F [4]= 36;
+		F [7]= 126;
+		F [10]= 5;
+		
+		
+		// 10. Declare un vector N[12] y realizar lo siguiente N[0] el promedio de 3,10,9 N[4] la suma de 7,8,10 N[12] la multiplicacion de 5,7,3 
+		// N[20] la suma de 7,-3,8 y en la ultima posicion el valor de 20/4
+		
+		int K []=new int [12];
+		K [0]= 7-33;
+		K [4]= 25;
+		K [12]= 280;// Esta linea nos va a presentar un error
+		K [20]= 12; // Esta linea nos va a presentar un error 
+		K [12]= 280; //Esta linea nos va a presentar un error
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
