00bec26a7af9baf65532ff02245a85bad142a70c
Karlita <Karlita@DESKTOP-ID66OAK>
2021-08-04 20:05:32 -0500

    Matriz1 Mi taller

--- src/ejercicios/varios/vectores/matrices/MainMatrices1.java
+++ src/ejercicios/varios/vectores/matrices/MainMatrices1.java
@@ -7,25 +7,39 @@ public class MainMatrices1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 1. Declare una matriz [3,4] de tipo char y desarrolle un programa que vaya solicitando 
-		// cada una de las posiciones 
+		// cada una de las posiciones
 		
+		//Este es mi teclado
+		Scanner lector = new Scanner (System.in);
 		
-		char A[][]= new char [3][4];
+		System.out.println("Ingrese el numero de filas del vector ");
+		// el usuario digita el numero de filas y yo debo leer desde el teclado 
+		int longitudFila= lector.nextInt();
+		
+		System.out.println("Ingrese e numero de columnas del vector: ");
+		//El usuario digita el numero de colunmas y yo debo leer desde teclado
+		int longitudColumna= lector.nextInt();
+		
+		char A[][]= new char [longitudFila][longitudColumna];
 		for (int fila = 0;fila < 3; fila ++) {
 			for (int columna= 0;columna <4; columna++) {
 				System.out.println("el valor fila: "+ fila +"y columna: " + columna+ "es:"+A[fila][columna]);
 				// el usuario digita el valor por teclado y a continuacion lo leo
-				Scanner lector =new Scanner (System.in);
+		
 				char n =lector.next().charAt(0);
 				A[fila][columna]=n;
 				
-			}
-			
-			
+			}	
 		
 		}
+		for (int fila = 0;fila < longitudFila; fila ++) {
+			for (int columna= 0;columna < longitudColumna; columna++) {
+				System.out.println(A[fila][columna]+" ");
+				System.out.println("el valor fila: "+ fila +"y columna: " + columna+ "es:"+ A[fila][columna]);
 		
-
+			}
+			System.out.println();
+		}
 	}
 
 }
