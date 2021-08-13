5dc30e38cbf249b1a9988e7ed144eebb7057f158
KarlitaM-07 <86031066+KarlitaM-07@users.noreply.github.com>
2021-08-13 16:42:12 -0500

    Add files via upload

---
+++ src/Clientes.java
@@ -0,0 +1,13 @@
+package tallerparejas;
+
+public class Clientes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		tienda venta1=new tienda();
+		venta1.helados("Cantidad de helados", 3,25,2);
+		
+	}
+
+}
---
+++ src/tienda.java
@@ -0,0 +1,46 @@
+package tallerparejas;
+
+//2. Declaracion de importaciones
+
+//3. Comentarios
+//Clase que se encargara de vender productos de una tienda 
+
+
+// 4. Declaracion de la clase
+
+public class tienda {
+		
+		// 5.CONSTANTES
+		//6. VARIABLES
+		public String Helados;
+		public int  helados= 20;
+		
+		
+		// 7.CONSTRUCTOR 
+		public String tienda;
+		
+		// 8. metodos 
+		
+		public void tienda(String heladosS, String sabores, int cantidad , int precio ) {
+			
+			
+			System.out.println("Mi tiendita de helados");
+			System.out.println("------------");
+			System.out.println("Sabor del helado: "+ sabores);
+			System.out.println("Cantidad de helados: "+ cantidad);
+			System.out.println("Precio del helado: "+ precio);
+			System.out.println("-----------");
+			System.out.println("Vuelva pronto ");
+			
+		}
+
+		public void helados(String string, int i, int j, int k) {
+			// TODO Auto-generated method stub
+			
+			
+		}
+
+	}
+
+
+
